92c7a4860dfe38c3b6f5b1d026e9cd69ac821e82
Chixozhmiox <retrojas635@gmail.com>
2026-09-12 18:27:44 +0700

    test

--- src/main/java/net/chixozhmix/dnmmod/spell/fire/MeteorSwarmSpell.java
+++ src/main/java/net/chixozhmix/dnmmod/spell/fire/MeteorSwarmSpell.java
@@ -34,6 +34,8 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 import java.util.Optional;
 
+
+//Тест изменений
 @AutoSpellConfig
 public class MeteorSwarmSpell extends AbstractSpell {
     private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(DnMmod.MOD_ID, "meteor_swarm");
