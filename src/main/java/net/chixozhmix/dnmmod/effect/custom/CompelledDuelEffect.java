31806023d742a9652d0dde3a790c890bbc940f3b
Chixozhmiox <retrojas635@gmail.com>
2026-09-12 18:49:30 +0700

    test merge breanch

---
+++ src/main/java/net/chixozhmix/dnmmod/effect/custom/CompelledDuelEffect.java
@@ -0,0 +1,10 @@
+package net.chixozhmix.dnmmod.effect.custom;
+
+import net.minecraft.world.effect.MobEffect;
+import net.minecraft.world.effect.MobEffectCategory;
+
+public class CompelledDuelEffect extends MobEffect {
+    protected CompelledDuelEffect(MobEffectCategory pCategory, int pColor) {
+        super(MobEffectCategory.HARMFUL, 0xC9A00C);
+    }
+}
--- src/main/java/net/chixozhmix/dnmmod/spell/fire/MeteorSwarmSpell.java
+++ src/main/java/net/chixozhmix/dnmmod/spell/fire/MeteorSwarmSpell.java
@@ -34,7 +34,6 @@ import org.jetbrains.annotations.Nullable;
 import java.util.List;
 import java.util.Optional;
 
-
 @AutoSpellConfig
 public class MeteorSwarmSpell extends AbstractSpell {
     private final ResourceLocation spellId = ResourceLocation.fromNamespaceAndPath(DnMmod.MOD_ID, "meteor_swarm");
